66e5065256327992b7f058231d02b2cbf6492ec5
SANADA Euki <yuu.mat.930@gmail.com>
2025-11-03 12:56:04 +0000

    feat: update user associations to restrict deletion and add unique index on username

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2025_11_03_120100) do
+ActiveRecord::Schema[8.1].define(version: 2025_11_03_121000) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -71,6 +71,7 @@ ActiveRecord::Schema[8.1].define(version: 2025_11_03_120100) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["unlock_token"], name: "index_users_on_unlock_token", unique: true
     t.index ["username"], name: "index_users_on_username", unique: true
+    t.index ["username"], name: "index_users_on_username_unique", unique: true
   end
 
   create_table "votes", force: :cascade do |t|
