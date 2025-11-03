6cf7e384b2a25de3bbd91811f6a624494d99a52c
SANADA Euki <yuu.mat.930@gmail.com>
2025-11-03 04:41:48 +0000

    fix: add unique index to votes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2025_11_03_080000) do
+ActiveRecord::Schema[8.1].define(version: 2025_11_03_090000) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -77,6 +77,7 @@ ActiveRecord::Schema[8.1].define(version: 2025_11_03_080000) do
     t.datetime "updated_at", null: false
     t.bigint "user_id", null: false
     t.index ["proposal_id"], name: "index_votes_on_proposal_id"
+    t.index ["user_id", "proposal_id"], name: "index_votes_on_user_id_and_proposal_id_unique", unique: true
     t.index ["user_id"], name: "index_votes_on_user_id"
   end
 
