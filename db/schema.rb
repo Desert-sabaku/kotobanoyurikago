135b6f73f615d7e9530c8b4796062812093f9f30
SANADA Euki <yuu.mat.930@gmail.com>
2025-11-03 04:12:04 +0000

    feat: add `Votes` model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2025_11_03_040613) do
+ActiveRecord::Schema[8.1].define(version: 2025_11_03_040824) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -59,7 +59,18 @@ ActiveRecord::Schema[8.1].define(version: 2025_11_03_040613) do
     t.index ["username"], name: "index_users_on_username", unique: true
   end
 
+  create_table "votes", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.bigint "proposal_id", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["proposal_id"], name: "index_votes_on_proposal_id"
+    t.index ["user_id"], name: "index_votes_on_user_id"
+  end
+
   add_foreign_key "proposals", "subjects"
   add_foreign_key "proposals", "users"
   add_foreign_key "subjects", "users"
+  add_foreign_key "votes", "proposals"
+  add_foreign_key "votes", "users"
 end
